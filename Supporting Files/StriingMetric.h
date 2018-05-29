2ff8e959c6247549d434b24a1a7e6356b0404b73
Atsushi Nagase <a@ngs.io>
2018-05-30 01:48:37 +0900

    Manually edit Xcode project

---
+++ Supporting Files/StriingMetric.h
@@ -0,0 +1,7 @@
+@import Foundation;
+
+//! Project version number for StringMetric.
+FOUNDATION_EXPORT double StringMetricVersionNumber;
+
+//! Project version string for StringMetric.
+FOUNDATION_EXPORT const unsigned char StringMetricVersionString[];
